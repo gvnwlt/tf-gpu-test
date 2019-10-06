0b8db30a9d294844d94e6e7cc6863efcae48ad1e
G <gvnwltrs@protonmail.com>
2019-10-05 21:36:48 -0400

    Add files via upload

---
+++ tf_gpu_test.py
@@ -0,0 +1,19 @@
+#gpu with tensorflow backend test 
+
+import tensorflow as tf
+
+#see if gpu present
+with tf.Session() as sess:
+  devices = sess.list_devices()
+  print(devices)
+
+#test gpu
+with tf.device('/gpu:0'):
+    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+    c = tf.matmul(a, b)
+
+with tf.Session() as sess:
+    print (sess.run(c))
+    print('gpu used')
+    
